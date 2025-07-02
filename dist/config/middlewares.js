f5738a6ea6b2fe29100c881d4903615d453561c8
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-07-02 11:56:45 +0200

    CORS config

--- dist/config/middlewares.js
+++ dist/config/middlewares.js
@@ -11,4 +11,23 @@ exports.default = [
     'strapi::session',
     'strapi::favicon',
     'strapi::public',
+    {
+        name: 'strapi::cors',
+        config: {
+            origin: [
+                'http://localhost:3000',
+                'https://finest-tobacco.com',
+                'https://www.finest-tobacco.com'
+            ],
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            headers: [
+                'Content-Type',
+                'Authorization',
+                'X-Frame-Options',
+                'X-Requested-With'
+            ],
+            credentials: true,
+            maxAge: 86400
+        }
+    }
 ];
